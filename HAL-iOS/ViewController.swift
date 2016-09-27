5bd64871d3058be54a6308e6117430b93bed1ba1
pranitha kota <pranitha.kota@macys.com>
2016-09-27 14:51:09 -0400

    Initial Commit

---
+++ HAL-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HAL-iOS
+//
+//  Created by Pranitha on 9/27/16.
+//  Copyright © 2016 macys. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

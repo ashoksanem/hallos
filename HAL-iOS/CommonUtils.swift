74ac9454d44ecdfecd67959914b3a36bc1278f43
b004602 <michael.flores@macys.com>
2016-10-31 15:50:12 -0400

    updated commonutils

--- HAL-iOS/CommonUtils.swift
+++ HAL-iOS/CommonUtils.swift
@@ -7,69 +7,79 @@
 //
 
 import Foundation
-class CommonUtils{
-    static let ssoUserDefaultKey = "SSOData";
+class CommonUtils
+{
     static let ssoSignedInKey = "ssoSignedInKey";
+    static let ssoAssociateInfo = "ssoAssociateInfo";
     static let currentPage = "currentPage";
-    class func setUpSSODefaults() -> Void {
+    
+    class func setUpSSODefaults() -> Void
+    {
         let defaults = UserDefaults.standard
-        defaults.setValue(nil, forKey: ssoUserDefaultKey)
         defaults.setValue(false, forKey: ssoSignedInKey)
-}
-    class func isSSOAuthenticated() -> Bool {
+        defaults.setValue([:], forKey: ssoAssociateInfo)
+    }
+    
+    class func isSSOAuthenticated() -> Bool
+    {
         let defaults = UserDefaults.standard
         return defaults.bool(forKey: ssoSignedInKey)
     }
     
     class func isSSOAuthenticatedMessage() -> String {
-        let successmsg = [
-            "signedin": "true"
-            ] as [String : Any]
         
-        let failmsg = [
-            "signedin": "false"
-            ] as [String : Any]
-        let halJsonData1 = try! JSONSerialization.data(withJSONObject: successmsg, options: [])
-        let halJsonData2 = try! JSONSerialization.data(withJSONObject: failmsg, options: [])
-        let  halJsonString1 = String(data: halJsonData1, encoding: String.Encoding.utf8)
-        let  halJsonString2 = String(data: halJsonData2, encoding: String.Encoding.utf8)
+        let defaults = UserDefaults.standard
+        let associate = defaults.dictionary(forKey: ssoAssociateInfo)! as [String:Any];
         
-        if(isSSOAuthenticated()){
-        return halJsonString1!
-        }
-        return halJsonString2!
-        
+        let associateData = try! JSONSerialization.data(withJSONObject: associate, options: [])
+        let associateString = String(data: associateData, encoding: String.Encoding.utf8)
+        return associateString!;
     }
-
     
     class func setIsSSOAuthenticated(value: Bool) -> Void {
         let defaults = UserDefaults.standard
         defaults.setValue(value, forKey: ssoSignedInKey)
+        
+        if( value == false ) {
+            defaults.setValue([:], forKey: ssoAssociateInfo)
+        }
     }
+    
     class func setCurrentPage(value: URL) -> Void {
         let defaults = UserDefaults.standard
         defaults.set(value, forKey: currentPage)
         //defaults.setValue(value, forKey: currentPage)
     }
+    
     class func getCurrentPage() -> URL {
         let defaults = UserDefaults.standard
         return defaults.url(forKey: currentPage)!
     }
+    
     class func setSSOData(value: Data) -> Void {
-        let defaults = UserDefaults.standard
-        defaults.setValue(value, forKey: ssoUserDefaultKey)
         
         let json = JSON(data: value)
         let ssoJsonObject : Json4Swift_Base = Json4Swift_Base(dictionary: json)!
-        print(ssoJsonObject.error?.errorCode)
-        if(ssoJsonObject.error?.errorCode==0)
+        
+        print(ssoJsonObject);
+        
+        let defaults = UserDefaults.standard
+        
+        if( ssoJsonObject.error?.errorCode == 0 )
         {
             CommonUtils.setIsSSOAuthenticated(value: true)
+            defaults.setValue(ssoJsonObject.dictionaryRepresentation(), forKey: ssoAssociateInfo)
         }
         else
         {
             CommonUtils.setIsSSOAuthenticated(value: false)
+            defaults.setValue([:], forKey: ssoAssociateInfo)
         }
-
+    }
+    
+    class func getSSOData() -> [String:Any]
+    {
+        let defaults = UserDefaults.standard
+        return defaults.value(forKey: ssoAssociateInfo) as! [String:Any]
     }
 }

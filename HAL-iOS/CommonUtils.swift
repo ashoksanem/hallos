8203b7f9a91a8fa220d3d8a890bf2072cdb68ef6
Pranitha Kota <b005308@a4731600.local>
2017-04-24 13:46:53 -0400

    changes to revert back landing page change

--- HAL-iOS/CommonUtils.swift
+++ HAL-iOS/CommonUtils.swift
@@ -157,8 +157,7 @@ class CommonUtils
     class func getLandingPage() -> URL
     {
         let defaults = UserDefaults.standard;
-        return Bundle.main.url(forResource: "HALApi/test", withExtension:"html")!
-        //return defaults.url(forKey: landingPage)!;
+        return defaults.url(forKey: landingPage)!;
             }
     
     class func setCurrentPage(value: URL) -> Void

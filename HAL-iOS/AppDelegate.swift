40fef7d3d5e0a648e14fda6afbe63a3a0ff9c5c8
Brian Dembinski <Brian.Dembinski@Macys.com>
2017-06-07 14:45:11 -0400

    SDU-674 Updated values to match what's currenly in prod.

--- HAL-iOS/AppDelegate.swift
+++ HAL-iOS/AppDelegate.swift
@@ -170,16 +170,16 @@ class AppDelegate: UIResponder, DTDeviceDelegate, UIApplicationDelegate {
     func setSimulatorValues()
     {
         CommonUtils.setLandingPage(value: URL(string: "http://mstore.devops.fds.com/")!);
-        CommonUtils.setAutoLogoutTimeinterval(value: 2500);
+        CommonUtils.setAutoLogoutTimeinterval(value: 3600);
         CommonUtils.setDivNum(value: 71);
         CommonUtils.setStoreNum(value: 572);
         SharedContainer.setIsp(value: "fs572asisp01");
         SharedContainer.setSsp(value: "fs008asssp01");
         SharedContainer.setCloud(value: "junk");
-        CommonUtils.setLogRetryCount(value: 25);
-        CommonUtils.setLogCountLimit(value: 25);
-        CommonUtils.setLogRetryFrequency(value: 25);
-        CommonUtils.setLogTimeLimit(value: 25);
+        CommonUtils.setLogRetryCount(value: 10);
+        CommonUtils.setLogCountLimit(value: 5);
+        CommonUtils.setLogRetryFrequency(value: 120);
+        CommonUtils.setLogTimeLimit(value: 120);
         
         let esp = ESPRequest();
         esp.getZipCode();
